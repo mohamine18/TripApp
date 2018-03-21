a43229b8f662bdef53c53c30fba9ef794b98028a
Med Amine <mohamine19@gmail.com>
2018-03-21 14:54:08 +0800

    1

---
+++ src/components/trips/TripDetails.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Trip Details</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import TripList from '@/components/trips/TripList'
+import TripDetails from '@/components/trips/TripDetails'
+import Login from '@/components/users/Login'
+import Profile from '@/components/users/Profile'
+import TripPayment from '@/components/trips/TripPayment'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: TripList
+    },
+    {
+      path: '/Trip-Details',
+      name: 'Trip Details',
+      component: TripDetails
+    },
+    {
+      path: '/Trip-Payment',
+      name: 'Trip Payment',
+      component: TripPayment
+    },
+    {
+      path: '/User-Profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
